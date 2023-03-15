dc73fe2d84b4362f8469306891e706c6ce7c78d8
vagrant <vagrant@vagrant.vm>
2023-03-15 16:35:34 +0000

    filter output to reduce text file size

---
+++ outputFilter.py
@@ -0,0 +1,43 @@
+
+class Analyzer:
+
+    def __init__(self,filename = "out.txt"):
+
+        self.lines = []
+        with open(filename,"r") as f:
+            self.lines = f.readlines()
+        
+    
+    def show(self):
+        for line in self.lines:
+            print(line)
+    
+    def len(self):
+        return len(self.lines)
+    
+    def filter(self,value):
+        i = 0
+        ans = []
+        while i < len(self.lines):
+            if value in self.lines[i]:
+                ans += [self.lines[i]]
+            i = i + 1
+        self.lines = ans
+    
+    def store(self,fname):
+        with open(fname,'w') as f:
+            for line in self.lines:
+                f.write(line)
+    
+
+
+
+if __name__ == "__main__":
+    a = Analyzer()
+    print(a.len())
+    a.filter('ssv-node-1')
+    print(a.len())
+    a.filter('$$$$$$')
+    print(a.len())
+    a.store('out2.txt')
+
